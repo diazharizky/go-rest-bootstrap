299b261097954c875e471385161bb8e8dd0de2d7
Diaz <diazharizky@gmail.com>
2025-09-18 22:31:39 +0700

    Replaced fullname with password

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        int64      `json:"id"`
 	Email     string     `json:"email"`
-	FullName  string     `json:"fullName"`
+	Password  string     `json:"password"`
 	CreatedAt time.Time  `json:"createdAt"`
 	UpdatedAt time.Time  `json:"updatedAt"`
 	DeletedAt *time.Time `json:"deletedAt,omitempty"`
--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 create table users (
     id serial primary key,
     email varchar(30) unique not null,
-    full_name varchar(30) not null,
+    password text not null,
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
     deleted_at timestamp
